4d8389f505c3988f97f912b6d7905df0e8e960ec
LeeTaeHeon <forever296@naver.com>
2023-06-28 15:25:39 +0900

    feature: loginpage logo

---
+++ petpal/src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../style/LoginPage.css";
+import Logo from '../components/Logo';
+
+const LoginPage = () => {
+    return (
+        <div id="loginParentDiv">
+            <div id="loginDiv">
+                <Logo />
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
