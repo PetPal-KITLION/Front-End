e30f634a84435cf447ff673d654b847e15d942d0
LeeTaeHeon <forever296@naver.com>
2023-07-06 20:06:51 +0900

    feat: tap content, img

--- petpal/src/components/Layout.jsx
+++ petpal/src/components/Layout.jsx
@@ -4,14 +4,14 @@ const Layout = ({ children }) => {
   return (
     <div>
       {/* 상단 이미지 */}
-      <img src={`${process.env.PUBLIC_URL}/image/layoutTop.png`} style={{}} alt="상단 이미지" />
+      <img src={`${process.env.PUBLIC_URL}/image/layoutTop.png`} style={{width:"25%", height:"25%"}} alt="상단 이미지" />
 
       {/* 모든 페이지 내용 */}
       {children}
 
       {/* 하단 이미지 */}
-      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <img src={`${process.env.PUBLIC_URL}/image/layoutBot.png`} alt="하단 이미지" />
+      <div style={{display: "flex", justifyContent: "flex-end" }}>
+        <img src={`${process.env.PUBLIC_URL}/image/layoutBot.png`} alt="하단 이미지" style={{width:"25%", height:"25%"}} />
       </div>
     </div>
   );
