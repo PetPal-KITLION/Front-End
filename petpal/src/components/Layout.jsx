230185edffd656329285a25b9415078c7bde17f0
Je-DG <gyuni6652@naver.com>
2023-07-06 22:08:43 +0900

    fix: Layout fix

--- petpal/src/components/Layout.jsx
+++ petpal/src/components/Layout.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from "react";
 import "../style/Layout.css";
 
 const Layout = ({ children }) => {
   return (
     <div id="layoutDiv">
       {/* 상단 이미지 */}
-      <img src={`${process.env.PUBLIC_URL}/image/layoutTop.png`} style={{width:"25%", height:"25%"}} className="layoutDivImg" alt="상단 이미지" />
+      <img
+        src={`${process.env.PUBLIC_URL}/image/layoutTop.png`}
+        style={{ width: "20%", height: "20%" }}
+        className="layoutDivImg"
+        alt="상단 이미지"
+      />
 
       {/* 모든 페이지 내용 */}
       {children}
 
       {/* 하단 이미지 */}
-      <div style={{display: "flex", justifyContent: "flex-end" }}>
-        <img src={`${process.env.PUBLIC_URL}/image/layoutBot.png`} alt="하단 이미지" className="layoutDivImg" style={{width:"25%", height:"25%"}} />
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <img
+          src={`${process.env.PUBLIC_URL}/image/layoutBot.png`}
+          alt="하단 이미지"
+          className="layoutDivImg"
+          style={{ width: "25%", height: "25%" }}
+        />
       </div>
     </div>
   );
