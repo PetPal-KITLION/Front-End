b91e95ba77f1e47c9e3248a46066a5156287145a
LeeTaeHeon <forever296@naver.com>
2023-07-06 22:00:25 +0900

    feat: layout float

--- petpal/src/components/Layout.jsx
+++ petpal/src/components/Layout.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import "../style/Layout.css";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div id="layoutDiv">
       {/* 상단 이미지 */}
-      <img src={`${process.env.PUBLIC_URL}/image/layoutTop.png`} style={{width:"25%", height:"25%"}} alt="상단 이미지" />
+      <img src={`${process.env.PUBLIC_URL}/image/layoutTop.png`} style={{width:"25%", height:"25%"}} className="layoutDivImg" alt="상단 이미지" />
 
       {/* 모든 페이지 내용 */}
       {children}
 
       {/* 하단 이미지 */}
       <div style={{display: "flex", justifyContent: "flex-end" }}>
-        <img src={`${process.env.PUBLIC_URL}/image/layoutBot.png`} alt="하단 이미지" style={{width:"25%", height:"25%"}} />
+        <img src={`${process.env.PUBLIC_URL}/image/layoutBot.png`} alt="하단 이미지" className="layoutDivImg" style={{width:"25%", height:"25%"}} />
       </div>
     </div>
   );
