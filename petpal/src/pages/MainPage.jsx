565e34b3c35a7b901d6f254f038666f9318aa95a
Je-DG <gyuni6652@naver.com>
2023-07-06 22:28:34 +0900

    feat&fix : MainPage add fullPage, Dropdowun fix

--- petpal/src/pages/MainPage.jsx
+++ petpal/src/pages/MainPage.jsx
@@ -6,17 +6,30 @@ import Main2 from "../components/Main2";
 import Main3 from "../components/Main3";
 import Main4 from "../components/Main4";
 import Main5 from "../components/Main5";
+import { SectionsContainer, Section } from "react-fullpage";
 
 const MainPage = () => {
+  const options = {
+    anchors: ["sectionOne", "sectionTwo", "sectionThree", "sectionFour"],
+  };
+
   return (
-    <div>
-      <Header />
-      <Main1 />
-      <Main2 />
-      <Main3 />
-      <Main4 />
-      <Main5 />
-    </div>
+    <SectionsContainer {...options}>
+      <Section>
+        <Header />
+        <Main1 />
+      </Section>
+      <Section>
+        <Main2 />
+      </Section>
+      <Section>
+        <Main3 />
+      </Section>
+      <Section>
+        <Main4 />
+        <Main5 />
+      </Section>
+    </SectionsContainer>
   );
 };
 export default MainPage;
